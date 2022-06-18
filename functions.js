86c2d70f4e1c239f01a1ed6f933806bc4683fd88
Faith <fhgray96@gmail.com>
2022-06-17 21:45:01 -0500

    addfunction

--- functions.js
+++ functions.js
@@ -11,3 +11,8 @@ function capitalize(str) {
 
 module.exports = capitalize;
 
+function reverseString(word) {
+    for (let i = word.length -1; i >= 0; i--) {
+        //push word[i] into a word or into an array then change it to a word
+    }
+}
