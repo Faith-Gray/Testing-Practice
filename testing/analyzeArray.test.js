e8806fb0654a2e5ff5c24886bbb2d7198d52d45d
Faith <fhgray96@gmail.com>
2022-06-22 21:40:05 -0500

    added test/solution

---
+++ testing/analyzeArray.test.js
@@ -0,0 +1,11 @@
+import analyzeArray from '../codes/analyzeArray';
+
+
+test('Analyze array and return the average, min, max, and length', () => {
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+    });
+})
